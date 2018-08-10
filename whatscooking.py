132694379e63f40265bcc8c40672bb04e7633898
eeghor <eeghor@gmail.com>
2018-08-10 23:42:45 +1000

    small changes

--- whatscooking.py
+++ whatscooking.py
@@ -7,6 +7,7 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 from collections import Counter
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.pipeline import make_pipeline, make_union
+from itertools import chain
 
 from sklearn.base import BaseEstimator, TransformerMixin
 
@@ -16,8 +17,9 @@ class C1(BaseEstimator, TransformerMixin):
 				return self
 
 		def transform(self, X, **transform_params):
-				print(X.str.split('.').tolist())
-				return X.str.split('.').tolist()
+				lst = [_.strip() for _ in chain.from_iterable(X.str.split('.').tolist())]
+				print(lst)
+				return lst
 
 
 class Coookings:
@@ -42,13 +44,13 @@ class Coookings:
 
 	def split(self):
 
-		X = self.train_['ingredients']
-		y = self.train_['cuisine']
+		X = self.train_[['ingredients']]
+		y = self.train_[['cuisine']]
 
 		self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, 
 					test_size=0.3, random_state=42, stratify=y)
 
-		print(self.X_train.head())
+		print(self.X_train[self.y_train.isnull()])
 
 		return self
 
@@ -61,7 +63,7 @@ class Coookings:
 		# print(cv.vocabulary_)
 
 
-		pipeline = make_pipeline(C1(), CountVectorizer(strip_accents='ascii', analyzer='word', ngram_range=(1,2)),
+		pipeline = make_pipeline(CountVectorizer(strip_accents='ascii', analyzer='word', ngram_range=(1,2)),
 															RandomForestClassifier())
 		# print(len(self.X_train.shape))
 		# print(self.y_train.shape)
