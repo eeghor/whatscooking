1387bcf1c4b30900c9916a7b3914534661ffee3c
Unknown <eeghor@gmail.com>
2018-08-29 23:56:55 +1000

    trying to fix singleton problem

--- whatscooking.py
+++ whatscooking.py
@@ -26,102 +26,34 @@ class Coookings:
 
 	def __init__(self):
 
-		self.train_data = json.load(open('data/train.json'))
-		self.test_data = json.load(open('data/test.json'))
+		self.train_data = pd.read_json('data/train.json', orient='records')
+		self.test_data = pd.read_json('data/test.json', orient='records')
 
 		# note that test data looks similar to train data but there are NO cuisine fields
 
-		cuis_ = [_['cuisine'] for _ in self.train_data]
-		ids_ = {_['id'] for _ in self.train_data}
-		ingrs_ = {i.lower().strip() for _ in self.train_data for i in _['ingredients']}
-
-		print(f'records: {len(self.train_data):,}\ncuisines: {len(set(cuis_))}\ningredients: {len(ingrs_):,}')
-		print(sorted([(k, v) for k, v in Counter(cuis_).items()], key=lambda _: _[1], reverse=True))
-
-		self.train_ = pd.DataFrame.from_dict([{'id': r['id'], 
-																						'ingredients': '. '.join(r['ingredients']), 'cuisine': r['cuisine']}  
-							for r in self.train_data])
-
 	def split(self):
 
-		X = self.train_[['ingredients']]
-		y = self.train_[['cuisine']]
+		X = self.train_data['ingredients'].str.join('. ')
+		y = self.train_data['cuisine']
 
 		self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, 
 					test_size=0.3, random_state=42, stratify=y)
 
-		print(self.X_train[self.y_train.isnull()])
+		print(f'testing set: {self.X_train.shape}')
+
 
 		return self
 
 	def train_model(self):
 
-		# cv = CountVectorizer(strip_accents='ascii', analyzer='word', ngram_range=(1,2))
-
-		# cv.fit(self.X_train['ingredients'].str.split('.'))
-
-		# print(cv.vocabulary_)
-
-
 		pipeline = make_pipeline(CountVectorizer(strip_accents='ascii', analyzer='word', ngram_range=(1,2)),
 															RandomForestClassifier())
-		# print(len(self.X_train.shape))
-		# print(self.y_train.shape)
 
-		# d = self.X_train['ingredients'].tolist()
-
-		# print(d[:20])
-
-		# print(self.y_train[:20])
-
+		print(self.X_train.head())
+		print(self.X_train.shape)
+		print(len(self.X_train.shape))
 		pipeline.fit(self.X_train)
-"""
-	{
-		"id": 10259,
-		"cuisine": "greek",
-		"ingredients": [
-			"romaine lettuce",
-			"black olives",
-			"grape tomatoes",
-			"garlic",
-			"pepper",
-			"purple onion",
-			"seasoning",
-			"garbanzo beans",
-			"feta cheese crumbles"
-		]
-	},
-"""
 
 if __name__ == '__main__':
 
 	cook = Coookings().split().train_model()
-
-# tr = []
-
-# for r in train_data:
-# 	main_ingrs = {i: 1 for i in r['ingredients']}
-# 	more_ingrs = {w: 1 for i in main_ingrs for w in i.split() if w.isalpha()}
-# 	tr.append({**{'id': r['id'], 'cuisine': r['cuisine']}, 
-# 					**main_ingrs, **more_ingrs})
-
-# le = LabelEncoder()
-
-# train_data_df = pd.DataFrame.from_dict(tr).fillna(0)
-
-# le.fit(train_data_df['cuisine'])
-
-# train_data_df['cuisine'] = le.transform(train_data_df['cuisine'])
-
-
-
-# print(X_train.shape)
-
-# clf = RandomForestClassifier(max_depth=2, random_state=5)
-
-# clf.fit(X_train, y_train)
-
-# y_pred = clf.predict(X_test)
-
-# print(accuracy_score(y_test, y_pred))
-
